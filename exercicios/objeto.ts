329f827aee19af4a575c77d84ebaa6e0738ae8c0
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-04-19 08:41:57 -0300

    objeto ts

---
+++ exercicios/objeto.ts
@@ -0,0 +1,4 @@
+// let objeto: { nome: string, idade: number, ehLegal: boolean }
+// objeto = { nome: "João", idade: 27, ehLegal: true }
+
+const obj: Record<string, string> = { nome: "Joao", sexo: "Masculino" }
